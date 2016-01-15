121137d1aa163741977170091aef1669f4079b72
Will King <willking@Wills-MacBook-Pro.local>
2016-01-14 23:23:23 -0600

    progress commit: partial nav set up

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,21 @@ var prefix      = require('gulp-autoprefixer');
 var cssnano     = require('gulp-cssnano');
 var htmlmin     = require('gulp-htmlmin');
 var uglify      = require('gulp-uglify');
-var notify     = require('gulp-notify');
+var del         = require('del');
 var reload      = browserSync.reload;
 
-// Paths for source
-var scss_source = './source/scss/',
-    css_source  = './source/css/',
-    jade_source = './source/jadefiles/',
-    source      = './source';
-
-// Paths for build
-var css_build = './build/css/',
-    build = './build/';
+// Delete files in build before rebuild
+// ------------------------------------
+gulp.task('clean:build', function () {
+  return del([
+    'build/**',
+  ]);
+});
 
 // Compile jade files to minified html
 // ------------------------------------
 gulp.task('jade', function () {
-  return gulp.src('source/jadefiles/**/*.jade')
+  return gulp.src('source/jadefiles/index.jade')
     .pipe(jade({
       pretty: true
     }))
@@ -30,7 +28,6 @@ gulp.task('jade', function () {
     .pipe(htmlmin())
     .pipe(gulp.dest('build/'))
     .pipe(reload({stream:true}))
-    .pipe(notify('Jade task complete!'));
 });
 
 // Compile SCSS + autoprefixer
@@ -44,7 +41,6 @@ gulp.task('css', function () {
     .pipe(prefix('last 2 versions'))
     .pipe(gulp.dest('source/css/'))
     .pipe(reload({stream:true}))
-    .pipe(notify('Css task complete!'));
 });
 
 // Remove unused css
@@ -53,23 +49,22 @@ gulp.task('uncss', function () {
   return gulp.src('source/css/main.css')
    .pipe(cssnano())
    .pipe(gulp.dest('build/css'))
-   .pipe(notify('css has been minified'));
 });
 
 
 
 // Build for dev and prod
 // ----------------------------------------------------------------------------
-gulp.task('dev', ['jade', 'css']);
+gulp.task('dev', [ 'clean:build', 'jade', 'css']);
 
-gulp.task('prod', ['jade', 'css', 'uncss']);
+gulp.task('prod', ['clean:build', 'jade', 'css', 'uncss']);
 
 
 // Watch
 // ----------------------------------------------------------------------------
 gulp.task('watch', ['prod'], function () {
   browserSync.init({
-    server: build
+    server: 'build'
   })
   gulp.watch('source/**/*.jade', ['jade']);
   gulp.watch('source/**/*.scss', ['css']);
